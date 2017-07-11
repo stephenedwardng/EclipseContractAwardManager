0efc43ce4367edd6ab312f41032a9e56467f6d1e
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-07-11 11:25:30 +0100

    Hero added

--- src/main/UserInterface.java
+++ src/main/UserInterface.java
@@ -2,7 +2,11 @@ package main;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class UserInterface extends JFrame {
@@ -28,6 +32,17 @@ public class UserInterface extends JFrame {
 		headerLabel.setForeground(lightBlue);
 		panel.add(headerLabel);
 		
+		BufferedImage heroImage; 
+		JLabel picture1 = new JLabel();
+		
+		
+		try {
+		heroImage = ImageIO.read(new File("/Users/user/eclipse-workspace/heroImage.jpg"));
+		picture1 = new JLabel(new ImageIcon(heroImage));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		panel.add(picture1);
 		
 		
 		this.setLocationRelativeTo(null);
